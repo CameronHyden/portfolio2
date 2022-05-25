4d603075fc15d498065f5b49774fd2f32421f967
CameronHyden <camh.hyden@gmail.com>
2022-05-25 12:57:34 +0100

    updated my projects and refactored data

---
+++ src/assets/data/data.js
@@ -0,0 +1,64 @@
+import Game from "../../assets/images/game.png";
+import Calculator from "../../assets/images/calculator.png";
+import BrewDog from "../../assets/images/brewDog.png";
+import Git from "../../assets/images/github.png";
+import Morse from "../../assets/images/morse.png";
+import positivePup from "../../assets/images/positivePup.png";
+import codeLog from "../../assets/images/codeLog.png";
+
+export const data = [
+  {
+    id: "1",
+    icon: Git,
+    title: "Alien invaders",
+    desc: "An alien invader style shooter created with vanilla JS, SASS and HTML.",
+    img: Game,
+    url: "https://github.com/CameronHyden/java-script-miniGame",
+    liveSite: "https://cameronhyden.github.io/java-script-miniGame/",
+  },
+  {
+    id: "2",
+    icon: Git,
+    title: "JavaScript calculator",
+    desc: " A functioning calculator created with JS,SASS and HTML, tested with cypress",
+    img: Calculator,
+    url: "https://github.com/CameronHyden/calculator-java",
+    liveSite: "https://cameronhyden.github.io/calculator-java/",
+  },
+  {
+    id: "3",
+    icon: Git,
+    title: "BrewDog API",
+    desc: " A BrewDog beer finder application created using their API through REACT",
+    img: BrewDog,
+    url: "https://github.com/CameronHyden/punk-api",
+    liveSite: "https://cameronhyden.github.io/punk-api/",
+  },
+  {
+    id: "4",
+    icon: Git,
+    title: "Morse code translator",
+    desc: " A morse code translator, made with javaScript. Functionality to translate morse to english",
+    img: Morse,
+    url: "https://github.com/CameronHyden/morse-code-translator-",
+    liveSite: "https://cameronhyden.github.io/morse-code-translator-/",
+  },
+  {
+    id: "5",
+    icon: Git,
+    title: "Positive Pup",
+    desc: "A 4 week client project, working in an agile team of 8 to build a new website for a puppy training company",
+    img: positivePup,
+    url: "https://github.com/nology-tech/positive-pup",
+    liveSite: "https://nology-tech.github.io/positive-pup/",
+  },
+  {
+    id: "6",
+    icon: Git,
+    title: "Positive Pup",
+    desc: "A full stack application, with spring backend application, deployed to GCP",
+    img: codeLog,
+    url: "https://github.com/CameronHyden/code-log",
+    liveSite: "https://cameronhyden.github.io/code-log/",
+  },
+];
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,62 +1,10 @@
 import "./Carousel.scss";
 import Down from "../../assets/images/down.png";
 import React, { useState } from "react";
-import Game from "../../assets/images/game.png";
-import Calculator from "../../assets/images/calculator.png";
-import BrewDog from "../../assets/images/brewDog.png";
-import Git from "../../assets/images/github.png";
-import Morse from "../../assets/images/morse.png";
-import positivePup from "../../assets/images/positivePup.png";
+import { data } from "../../assets/data/data";
 
 const Carousel = () => {
   const [currentSlider, setCurrentSlider] = useState(0);
-  const data = [
-    {
-      id: "1",
-      icon: Git,
-      title: "Alien invaders",
-      desc: "An alien invader style shooter created with vanilla JS, SASS and HTML.",
-      img: Game,
-      url: "https://github.com/CameronHyden/java-script-miniGame",
-      liveSite: "https://cameronhyden.github.io/java-script-miniGame/",
-    },
-    {
-      id: "2",
-      icon: Git,
-      title: "JavaScript calculator",
-      desc: " A functioning calculator created with JS,SASS and HTML, tested with cypress",
-      img: Calculator,
-      url: "https://github.com/CameronHyden/calculator-java",
-      liveSite: "https://cameronhyden.github.io/calculator-java/",
-    },
-    {
-      id: "3",
-      icon: Git,
-      title: "BrewDog API",
-      desc: " A BrewDog beer finder application created using their API through REACT",
-      img: BrewDog,
-      url: "https://github.com/CameronHyden/punk-api",
-      liveSite: "https://cameronhyden.github.io/punk-api/",
-    },
-    {
-      id: "4",
-      icon: Git,
-      title: "Morse code translator",
-      desc: " A morse code translator, made with javaScript. Functionality to translate morse to english",
-      img: Morse,
-      url: "https://github.com/CameronHyden/morse-code-translator-",
-      liveSite: "https://cameronhyden.github.io/morse-code-translator-/",
-    },
-    {
-      id: "5",
-      icon: Git,
-      title: "Positive Pup",
-      desc: "A 4 week client project, working in an agile team of 8 to build a new website for a puppy training company",
-      img: positivePup,
-      url: "https://github.com/nology-tech/positive-pup",
-      liveSite: "https://nology-tech.github.io/positive-pup/",
-    },
-  ];
 
   const handleClick = (direction) => {
     direction === "left"
